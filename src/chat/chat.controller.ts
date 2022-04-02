1b0aa14b06f96b0091c45bafe79418436b89d203
IgorTikush <igortikush@mail.ru>
2022-04-02 21:48:07 +0800

    added websocket for chat

---
+++ src/chat/chat.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { ChatService } from './chat.service';
+// import { AppService } from './app.service';
+
+@Controller('chat')
+export class ChatController {
+  constructor(private readonly chatService: ChatService) {}
+
+  @Get()
+  getHello(): string {
+    return this.chatService.getHello();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import * as config from 'config';
+import { Server } from 'socket.io'
+
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+
+  await app.listen(config.get('port'));
+}
+bootstrap();
